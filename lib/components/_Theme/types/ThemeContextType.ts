5225441e0c15f10845d0391fc06c0534def3e318
alex01aik <alexashka010100@gmail.com>
2024-03-03 21:43:00 +0100

    theme fix

--- lib/components/_Theme/types/ThemeContextType.ts
+++ lib/components/_Theme/types/ThemeContextType.ts
@@ -1,4 +1,4 @@
 export type ThemeContextType<T> = {
-  changeTheme: (key: string | T) => void;
-  theme: string | T;
+  changeTheme?: (key: string | T) => void;
+  theme?: string | T;
 };
--- lib/components/_Theme/useTheme.ts
+++ lib/components/_Theme/useTheme.ts
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { ThemeContextType } from "./types/ThemeContextType";
 import { ThemeContext } from "./ThemeProvider";
 
-export const useTheme = (): ThemeContextType => {
+export const useTheme = (): ThemeContextType<any> => {
   const context = useContext(ThemeContext);
   if (!context) {
     throw new Error("useTheme must be used within a ThemeProvider");
